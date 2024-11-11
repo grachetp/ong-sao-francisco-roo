47d310c20ae09408fcfe6bc5b1673e918cdfe3cf
Pedro Grachet <pedro.grachet@gmail.com>
2024-11-10 20:06:31 -0400

    feat: added initial struct

---
+++ script.js
@@ -0,0 +1,4 @@
+function showContactInfo() {
+    const contactInfo = document.getElementById("contact-info");
+    contactInfo.style.display = contactInfo.style.display === "none" ? "block" : "none";
+}
